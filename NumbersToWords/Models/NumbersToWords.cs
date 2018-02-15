da007c0d0ee6234659edd48dc1fc3372e50148a5
Cameron Anderson and dalia ramos <cameron@>
2018-02-15 13:30:52 -0800

    add readme

---
+++ NumbersToWords/Models/NumbersToWords.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+
+namespace NumbersToWords.Models
+{
+  public class NumberToWord
+  {
+
+
+    public static bool Test(bool b)
+    {
+      return b;
+    }
+  }
+}
